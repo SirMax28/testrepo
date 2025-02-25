ccd5399434134a6cecbefafbdec35ecd3d61619f
SirMax28 <samuelbernal310@gmail.com>
2025-02-25 12:04:03 -0500

    nueva api

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string stripe_api_key="sk_test_51J3JZwJ9Q6Z";   // stripe api key
+string stripe_api_key="sk_test_514eyjDarT1J3JZwJ9Q6Zdc";  
